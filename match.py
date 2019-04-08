595afbb3bf72c308c770be9cdd089a5b3f18ca4f
Michael Davis <mcd164@miami.edu>
2019-04-08 16:42:20 -0400

    Add random seeder

--- match.py
+++ match.py
@@ -1,4 +1,13 @@
 #! /usr/bin/env python
 
+import random
+import argparse
+
 if __name__ == '__main__':
-    print("hello")
+    random_seed = random.randint(1, 262571)
+    parser = argparse.ArgumentParser(description="Cuda based correspondence grouping")
+    parser.add_argument('--seed', dest='seed', type=int, default=random_seed,
+                        help="Seed value for the random generator")
+    args = parser.parse_args()
+
+    random.seed(args.seed)
