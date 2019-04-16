959248c5976dbfa7aaf91ff9f989d4ba588da98d
Michael Davis <mcd164@miami.edu>
2019-04-16 18:42:47 -0400

    Correct the nearest neighbor

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -4,10 +4,23 @@ def nearest_neighbor(dataset):
     a, b = dataset
     nn = np.zeros((a.size/3,))
     for i in range(0, a.size/3):
-        dists = np.array([euclidean_dist(np.squeeze(np.asarray(a[:, i])), xyz) for xyz in np.squeeze(np.asarray(b))])
         # returns the index of the minimum element in the array
-        nn[i] = np.argmin(dists)
+        dists = [euclidean_dist(xyz(a, i), squash(b_j)) for b_j in b.T]
+
+        index = 0
+        min_elem = float('inf')
+        for j in range(0, len(dists)):
+            if dists[j] < min_elem:
+                min_elem = dists[j]
+                index = j
+        nn[i] = index
     return nn
+
+def xyz(matrix, entry):
+    return squash(matrix[:, entry])
+
+def squash(matrix):
+    return np.squeeze(np.asarray(matrix))
 
 def euclidean_dist(p_a, p_b):
     return np.sqrt((p_b[0] - p_a[0]) ** 2 + (p_b[1] - p_a[1]) ** 2)
